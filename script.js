991e83f1a2191159d21a8fa9b6b79968c2a6f3ab
Ejeh Achiever Iduh <110111012+Achiever011@users.noreply.github.com>
2026-07-20 09:48:36 +0100

    Add files via upload
    MOBILE VIEW

---
+++ script.js
@@ -0,0 +1,27 @@
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('header');
+//     if (window.scrollY > 50) {
+//         header.classList.add('scrolled');
+//     } else{
+//         header.classList.remove('scrolled');
+//     }
+//     });
+
+/* FORM TO MY EMAIL   */
+<script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+<script>
+  emailjs.init("YOUR_PUBLIC_KEY");
+
+  document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+      .then(() => {
+        alert("Message sent!");
+        this.reset();
+      })
+      .catch((err) => {
+        alert("Something went wrong. Please try again.");
+        console.error(err);
+      });
+  });
+</script>
